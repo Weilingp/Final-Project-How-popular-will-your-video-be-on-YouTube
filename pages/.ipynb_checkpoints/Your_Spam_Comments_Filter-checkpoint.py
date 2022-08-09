b2a6f6debce6ea3a015b6fe477392879b4b9b440
Weilingp <pwl25@hotmail.com>
2022-08-09 23:46:52 +0200

    spam files updated

--- pages/.ipynb_checkpoints/Your_Spam_Comments_Filter-checkpoint.py
+++ pages/.ipynb_checkpoints/Your_Spam_Comments_Filter-checkpoint.py
@@ -13,14 +13,14 @@ comments = pd.read_csv('spam_perc_by_videoid.csv')
 
 
 
-df = comments[["video_id", "video_count", "predictions", "perc"]]#.reset_index(inplace= True)
+df = comments[["video_id", "total_videos", "total_spam_comments", "perc"]]#.reset_index(inplace= True)
 
 #st.dataframe(df.head())
 #input the video we want to analyse
 
-video = st.text_input('Type The VideoID to view the percentage of spam comments', 'jt2OHQh0HoQ')
+video = st.text_input('Type The VideoID to view the percentage of spam comments', 'VideoID')
 
-st.write('This is the classifications of the comments of the video', video)
+#st.write('This is the classifications of the comments of the video', video)
 
 videoid = str(video)
 
@@ -32,26 +32,24 @@ def ploting_spam_comments_per_video(videoid):
     st.dataframe(video)
     
     
-    
-    #fig, ax = plt.subplots(figsize=(8,8))
-    
-    #video.plot.bar()
   
-    
-    st.bar_chart(video[["video_count", "predictions", "perc"]])
-    
-   # st.pyplot(fig)
+    fig = video.plot.bar()
+    st.pyplot()
+   
     return video
 
-st.header("Percentage of spam comments")
- 
-if st.button('Click Here'):
-    
+st.write("""
+#### Percentage of spam comments
+***
+""")
+
+
+if st.button('Comments'):
     viewer_sentiments = ploting_spam_comments_per_video(videoid)
     #st.text(channel_choise)
     st.table(viewer_sentiments)
 
 else:
-     st.write('predict the percentage of Spam comments!')
+    st.write('predict the percentage of Spam comments!')
 
 
