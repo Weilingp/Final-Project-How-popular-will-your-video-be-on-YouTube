7a290cdf6d52322934ecba09706ffd58c2dc0414
Weilingp <pwl25@hotmail.com>
2022-08-24 22:44:09 +0200

    remove png files

--- pages/Your_Spam_Comments_Filter.py
+++ pages/Your_Spam_Comments_Filter.py
@@ -10,10 +10,11 @@ st.set_page_config(page_title = "Comment Analysis", page_icon='▶️')
 st.title("Sentiments Analysis on Comments Per video")
 
 comments = pd.read_csv('spam_perc_by_videoid.csv')
-
-
+non_spam_comments = pd.read_csv("non_spam_comments.csv")
 
 df = comments[["video_id", "total_videos", "total_spam_comments", "perc"]]#.reset_index(inplace= True)
+
+good_comment = non_spam_comments.loc[non_spam_comments["true_values"]==0, :]
 
 #st.dataframe(df.head())
 #input the video we want to analyse
@@ -31,7 +32,8 @@ def ploting_spam_comments_per_video(videoid):
     video = df.loc[df["video_id"] == videoid, :]
     st.dataframe(video)
     
-    
+    good_comments = good_comment.loc[good_comment["video_id"]==videoid, :]
+    st.dataframe(good_comments[["predictions", "true_values", "clean_comment"]].head())
   
     fig = video.plot.bar()
     st.pyplot()
